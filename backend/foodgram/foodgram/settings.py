e8be3fd33e979aa0dae4d9e2391e458c109688e2
Marat Ikhsanov <ihsmen@gmail.com>
2022-12-29 14:34:06 +0300

    Deploy

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ['*', ])
 
 AUTH_USER_MODEL = 'users.User'
 
