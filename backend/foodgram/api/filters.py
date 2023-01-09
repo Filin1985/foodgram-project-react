c16cb7f76f06473f49a9f3c33a051a63573b8aa9
Marat Ikhsanov <ihsmen@gmail.com>
2023-01-09 17:30:03 +0300

    Deploy

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -14,10 +14,17 @@ class CustomRecipeFilter(filters.FilterSet):
         queryset=User.objects.all()
     )
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='get_is_in_shopping_cart')
 
     def get_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
             return queryset.filter(favorites__user=self.request.user)
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(cart__user=self.request.user)
         return queryset
 
     class Meta:
